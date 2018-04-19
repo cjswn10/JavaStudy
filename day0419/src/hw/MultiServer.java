de5f57bf312be44b4fdd19e2ef0f904653771649
cjswn10 <cjswn10@gmail.com>
2018-04-19 23:26:53 +0900

    MultiServer 작성

---
+++ day0419/src/hw/MultiServer.java
@@ -0,0 +1,42 @@
+package hw;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class MultiServer {
+	private ArrayList<MultiServerThread> list;
+	private Socket socket;
+	
+	public MultiServer() throws IOException{
+		list = new ArrayList<MultiServerThread>();
+		ServerSocket serverSocket = new ServerSocket(5000);	//port
+		MultiServerThread mst = null;
+		
+		boolean isStop = false;
+		while (!isStop) {
+			System.out.println("Server ready...");
+			socket = serverSocket.accept();
+			mst = new MultiServerThread(this);
+			list.add(mst);
+			Thread t = new Thread(mst);
+			t.start();
+			
+		}
+		
+	}
+	
+	public ArrayList<MultiServerThread> getList() {
+		return list;
+	}
+	
+	public Socket getSocket() {
+		return socket;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		new MultiServer();
+	}
+
+}
---
+++ day0419/src/hw/MultiServerThread.java
@@ -0,0 +1,58 @@
+package hw;
+
+import java.io.*;
+import java.net.*;
+
+public class MultiServerThread implements Runnable {
+	private Socket socket;
+	private MultiServer ms;
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+	
+	public MultiServerThread(MultiServer ms) {
+		this.ms = ms;
+	}
+	
+	public synchronized void run() {
+		boolean isStop = false;
+		
+		try {
+			socket = ms.getSocket();
+			ois = new ObjectInputStream(socket.getInputStream());
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			
+			String message = null;
+			while(!isStop) {
+				message = (String)ois.readObject();
+				String[] str = message.split("#");
+				if(str[1].equals("exit")) {
+					broadCasting(message);
+					isStop = true;
+				} else {
+					broadCasting(message);
+				}
+			}
+			
+			ms.getList().remove(this);
+			System.out.println(socket.getInetAddress() + "정상적으로 종료하셨습니다");
+			System.out.println("list size: " + ms.getList().size());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			ms.getList().remove(this);
+			System.out.println(socket.getInetAddress() + "비정상적으로 종료하셨습니다");
+			System.out.println("list size: " + ms.getList().size());
+		}
+	}
+
+	private void broadCasting(String message) throws IOException {
+		for(MultiServerThread ct : ms.getList()) {
+			ct.send(message);
+		}
+	}
+
+	private void send(String message) throws IOException {
+		oos.writeObject(message);
+	}
+	
+	
+}
